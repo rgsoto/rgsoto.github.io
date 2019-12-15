5dfc21c4f2f1cc91c148a396e883732642048d06
Renier Soto <rgabrielsoto@gmail.com>
2019-12-15 08:32:20 -0500

    fix image gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,17 @@
+ // Activates image Gallery
+ // The main task is to attach an event listener to each image in th Gallery
+ // and respond appropirately on click
+
 function activateGallery() {
-  alert("Hell from the gallery file");
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                                            querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function(){
+      //set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+    });
+  });                                        
 }
